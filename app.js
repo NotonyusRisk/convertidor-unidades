370d6ee0634b6edce0ed8a056f858e39f1b50981
NotonyusRisk <antoniojrudass@gmail.com>
2024-02-27 12:21:46 -0500

    feat::app.js, funciones de conversion

--- app.js
+++ app.js
@@ -13,36 +13,54 @@ let numero = valor.value;
 //condicionales
 switch(election){
     case 1:
-        btnConvertir.addEventListener("click", celcius_fahrenheit);
+        btnConvertir.addEventListener("click", celcius_fahrenheit(numero));
         break;
     case 2:
-        btnConvertir.addEventListener("click", fahrenheit_celcius);
+        btnConvertir.addEventListener("click", fahrenheit_celcius(numero));
         break;
     case 3:
-        btnConvertir.addEventListener("click", celcius_kelvin);
+        btnConvertir.addEventListener("click", celcius_kelvin(numero));
         break;
     case 4:
-        btnConvertir.addEventListener("click", kelvin_celcius);
+        btnConvertir.addEventListener("click", kelvin_celcius(numero));
         break;
-    default:
-        alert("Opcion denegada.");
+    case 5:
+        btnConvertir.addEventListener("click", fahrenheit_kelvin(numero));
+        break;
+    case 6:
+        btnConvertir.addEventListener("click", kelvin_fahrenheit(numero));
+        break;
 }
 
 //funciones
-function celcius_fahrenheit() {
-    
+function celcius_fahrenheit(C) {
+    let F = (C * 9/5) + 32;
+    resultado.innerText = F;
 }
 
-function fahrenheit_celcius(){
-
+function fahrenheit_celcius(F){
+    let C = (F -32) * 5 / 9;
+    resultado.innerText = C;
 }
 
-function celcius_kelvin() {
-
+function celcius_kelvin(C) {
+    let K = C + 273;
+    resultado.innerText = K;
 }
 
-function kelvin_celcius() {
+function kelvin_celcius(K) {
+    let C = K - 273;
+    resultado.innerText = C;
+}
 
+function fahrenheit_kelvin(F) {
+    let K = (F + 459) * 5/9;
+    resultado.innerText = K;
+}
+
+function kelvin_fahrenheit(K) {
+    let F = (K * 9/5) -459;
+    resultado.innerText = F;
 }
 
 //pruebas
