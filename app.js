44ced8555fee7b813d08af4299208fdfd162443e
NotonyusRisk <antoniojrudass@gmail.com>
2024-02-28 17:07:08 -0500

    update

--- app.js
+++ app.js
@@ -7,68 +7,69 @@ let resultado = document.getElementById("resultado");
 let btnConvertir = document.getElementById("convertir");
 
 //variables
-let election = valueConvertion.selectedIndex;
-let numero = valor.value;
+let election = parseInt(valueConvertion.selectedIndex);
+let numero = parseInt(valor.value);
 
-//condicionales
-switch(election){
-    case 1:
-        btnConvertir.addEventListener("click", celcius_fahrenheit(numero));
-        break;
-    case 2:
-        btnConvertir.addEventListener("click", fahrenheit_celcius(numero));
-        break;
-    case 3:
-        btnConvertir.addEventListener("click", celcius_kelvin(numero));
-        break;
-    case 4:
-        btnConvertir.addEventListener("click", kelvin_celcius(numero));
-        break;
-    case 5:
-        btnConvertir.addEventListener("click", fahrenheit_kelvin(numero));
-        break;
-    case 6:
-        btnConvertir.addEventListener("click", kelvin_fahrenheit(numero));
-        break;
-}
-
+//eventos
 btnCerrar.onclick = () => {
     containerModal.style.display = none;
 }
+
+btnConvertir.addEventListener("click", () => {
+    switch(election){
+        case 0:
+            btnConvertir.addEventListener("click", () => {
+                alert("Opcion no valida.");
+            });
+        case 1:
+            celcius_fahrenheit(numero);
+            break;
+        case 2:
+            fahrenheit_celcius(numero);
+            break;
+        case 3:
+            celcius_kelvin(numero);
+            break;
+        case 4:
+            kelvin_celcius(numero);
+            break;
+        case 5:
+            fahrenheit_kelvin(numero);
+            break;
+        case 6:
+            kelvin_fahrenheit(numero);
+            break;
+    }
+    containerModal.style.display = block;
+});
 
 //funciones
 function celcius_fahrenheit(C) {
     let F = (C * 9/5) + 32;
     resultado.innerText = F;
-    containerModal.style.display = block;
 }
 
 function fahrenheit_celcius(F){
     let C = (F -32) * 5 / 9;
     resultado.innerText = C;
-    containerModal.style.display = block;
 }
 
 function celcius_kelvin(C) {
     let K = C + 273;
     resultado.innerText = K;
-    containerModal.style.display = block;
 }
 
 function kelvin_celcius(K) {
     let C = K - 273;
     resultado.innerText = C;
-    containerModal.style.display = block;
 }
 
 function fahrenheit_kelvin(F) {
     let K = (F + 459) * 5/9;
     resultado.innerText = K;
-    containerModal.style.display = block;
 }
 
 function kelvin_fahrenheit(K) {
     let F = (K * 9/5) -459;
     resultado.innerText = F;
-    containerModal.style.display = block;
 }
